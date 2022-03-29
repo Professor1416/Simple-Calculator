b0041b736d36b5f5244661a6c276e37ed3241405
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2022-03-29 20:37:17 +0530

    Add files via upload

---
+++ simple_calclulator/src/Simple_Calculator/Simple_Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Simple_Calculator;
+
+/**
+ *
+ * @author User
+ */
+public class Simple_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        S_cal c = new S_cal();
+        c.show();
+    }
+    
+}
